0b3ec98f92af007adcf33f722664602db775cbe0
Alex <alex.tilnenko@gmail.com>
2020-11-11 11:14:20 +0200

    Сверстал форму логина

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { reduxForm } from 'redux-form';
 
 function Login() {
    return (
-		<div className='login block'>
-			<div className='login__body'>
-				<a className='login__link'
-					href='https://social-network.samuraijs.com/'
-					target='_blank'
-					rel='noopener noreferrer'
-				>login</a>
-			</div>
-		</div>
-	);
+      <div className="login-wrapper block">
+         <form className="login-form">
+            <input
+               className="login-form__item login-form--input"
+               type="text"
+               placeholder="Логин"
+            ></input>
+            <input
+               className="login-form__item login-form--input"
+               type="password"
+               placeholder="Пароль"
+            ></input>
+            <div className="login-form__item">
+               <input
+                  className="login-form-checkbox"
+                  type="checkbox"
+                  id="remember"
+               />
+               <label className="login-form-label" htmlFor="remember">
+                  Запомнить меня
+               </label>
+            </div>
+            <button className="btn btn--login">Войти</button>
+         </form>
+      </div>
+   );
 }
 
-export default Login
+export default reduxForm({ form: 'login' })(Login);
